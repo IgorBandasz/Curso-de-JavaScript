873f45c7cec2b8fd243fbcad3036f8ecbe2b4fff
Igor Bandasz <44757501+IgorBandasz@users.noreply.github.com>
2020-07-22 21:53:01 -0300

    Seção 6 concluída

---
+++ 6-funcoes/8_argumentos_opcionais/script.js
@@ -0,0 +1,9 @@
+function nomeComIdade(nome, idade){
+    if(idade === undefined){
+        console.log("O nome é:"+nome);
+    } else {
+        console.log("O nome é:"+nome+"\nidade:"+idade);
+    }
+}
+
+nomeComIdade("igor");
